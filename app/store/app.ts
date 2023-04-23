d7575b39569a196f5c07f80839ca3640d751da4b
wxttt <wangxiaottt@gmail.com>
2023-04-23 11:20:56 +0800

    CHG: sendPreviewBubble to false

--- app/store/app.ts
+++ app/store/app.ts
@@ -143,7 +143,7 @@ const DEFAULT_CONFIG: ChatConfig = {
   fontSize: 14,
   theme: Theme.Auto as Theme,
   tightBorder: false,
-  sendPreviewBubble: true,
+  sendPreviewBubble: false,
   sidebarWidth: 300,
 
   disablePromptHint: false,
